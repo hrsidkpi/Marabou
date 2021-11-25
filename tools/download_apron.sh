313f8f49d2095b8e4282e72a0d72c6fb81bb07d2
Guy Katz <guykatz@cs.huji.ac.il>
2021-11-25 14:34:34 +0200

    guy's attempt to fix the compilation issues

--- tools/download_apron.sh
+++ tools/download_apron.sh
@@ -2,8 +2,8 @@
 curdir=$pwd
 
 # Download apron
- git clone https://github.com/antoinemine/apron.git
- cd apron
+git clone https://github.com/antoinemine/apron.git
+cd apron
 
 # Prepare the makefile and installation dir
 mkdir installed
@@ -13,5 +13,9 @@ patch < ../Makefile.config.patch
 # Build
 make -C . APRON_PREFIX=./installed MLGMPIDL_PREFIX=./installed JAVA_PREFIX=./installed
 make -C . install
+
+# A disgusting hack
+cd installed/include
+cp ../../taylor1plus/t1p_representation.h ../../taylor1plus/t1p_constructor.h ../../taylor1plus/t1p_meetjoin.h  ../../taylor1plus/t1p_assign.h ../../taylor1plus/t1p_resize.h ../../taylor1plus/t1p_otherops.h ../../taylor1plus/t1p_fun.h ../../taylor1plus/t1p_itv_utils.h  .
 
 cd $curdir
