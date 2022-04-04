ac45860da1b9ecc73703b61af4daf0fa61abb464
JordNGhelfN <yardenghelfan41@gmail.com>
2022-04-04 22:36:32 +0300

    build nad run all fix

--- build_and_run_all.sh
+++ build_and_run_all.sh
@@ -6,19 +6,20 @@ cd build
 echo "building..."
 cmake --build . -j 4
 
-cd ..
+cd .../run_batches
+ls
 echo "running all..."
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_1_p_1.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_1_p_2.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_1_p_3.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_1_p_4.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_1_p_1.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_1_p_2.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_1_p_3.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_1_p_4.sh
 
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_2_p_1.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_2_p_2.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_2_p_3.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_2_p_4.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_2_p_1.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_2_p_2.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_2_p_3.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_2_p_4.sh
 
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_3_p_1.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_3_p_2.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_3_p_3.sh
-sbatch --mem=400m -c4 --time=6:0:0 sbatch run_batches/n_1_3_p_4.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_3_p_1.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_3_p_2.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_3_p_3.sh
+sbatch --mem=400m -c4 --time=6:0:0 n_1_3_p_4.sh
