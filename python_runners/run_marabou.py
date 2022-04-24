77a35a801d58ea11cc60db735627a10c4a0ca596
JordNGhelfN <yardenghelfan41@gmail.com>
2022-04-24 18:22:14 +0300

    p1

--- python_runners/run_marabou.py
+++ python_runners/run_marabou.py
@@ -5,10 +5,10 @@ def run_marabou_ai(net_name_suffix: str, query_name_suffix: str, ai_interval: in
     net_name = f"../resources/nnet/acasxu/ACASXU_experimental_v2a_{net_name_suffix}.nnet"
     property_name = f"../resources/properties/acas_property_{query_name_suffix}.txt"
 
-    os.system('cd ../')
+    os.chdir('../')
     os.system('ls')
     os.system('mkdir -p build')
-    os.system('cd build')
+    os.chdir('build')
     os.system('module load gurobi')
     os.system('echo "building..."')
     os.system('cmake --build . -j 4')
@@ -17,7 +17,7 @@ def run_marabou_ai(net_name_suffix: str, query_name_suffix: str, ai_interval: in
     
 
 def run_marabou_no_ai(net_name_suffix: str, query_name_suffix: str):
-    os.system("cd ..")
+    os.chdir("..")
     os.system("./")
 
 
