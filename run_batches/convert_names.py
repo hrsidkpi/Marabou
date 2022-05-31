9c9e0aa8ff4031b3fff750405dc8c0ae9b8ad2c1
Jordan Ghelfan <32236686+hrsidkpi@users.noreply.github.com>
2022-05-31 11:20:40 +0300

    add run batch name converter

---
+++ run_batches/convert_names.py
@@ -0,0 +1,28 @@
+import os
+
+files = os.listdir()
+files.sort()
+
+last_c = "c1.out"
+last_c_num = 1
+for f in files:
+    if f[0] == 'c' and f[1:-4].isdigit() and f[-4:] == '.out':
+        print("ffff")
+        last_c_num = int(f[1:-4]) + 1
+        last_c = "c" + str(last_c_num) + ".out"    
+print(last_c_num)
+
+done = False
+while not done:
+    done = True
+
+    files = os.listdir()
+    files.sort()
+    for f in files:
+        if f.startswith('slurm'):
+            print(f, " -> ", last_c)
+            os.rename(f, last_c)
+            last_c_num += 1
+            last_c = "c" + str(last_c_num) + ".out"
+            done = False
+            break
