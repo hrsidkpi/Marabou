c9dcfca00c4e3df6494162902b62b0d0a830b3c6
Jordan Ghelfan <32236686+hrsidkpi@users.noreply.github.com>
2022-06-07 21:26:05 +0300

    n general with summary path

--- run_batches/n_general.sh
+++ run_batches/n_general.sh
@@ -12,7 +12,14 @@ prop_name_prefix="../resources/properties/acas_property_"
 prop_name_suffix=".txt"
 prop_name=$prop_name_prefix$3$prop_name_suffix
 
+summary_path_prefix="res_"
+summary_path_suffix=".txt"
+summary_path="${summary_path_prefix}${1}_${2}_${3}${summary_path_suffix}"
+
 echo "property: "
 echo $prop_name
 
-./Marabou $net_name $prop_name
+echo "summary path: "
+echo $summary_path
+
+./Marabou $net_name $prop_name --summary-file=$summary_path
