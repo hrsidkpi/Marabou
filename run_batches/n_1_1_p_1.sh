def3c24d6d4a11d08eb3ffcfd3e42089e8c5e19f
JordNGhelfN <yardenghelfan41@gmail.com>
2022-07-18 12:21:05 +0300

    run zonotope on all

--- run_batches/n_1_1_p_1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../build
-./Marabou ../resources/nnet/acasxu/ACASXU_experimental_v2a_1_1.nnet ../resources/properties/acas_property_1.txt 
--- run_batches/n_1_3_p_4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../build
-./Marabou ../resources/nnet/acasxu/ACASXU_experimental_v2a_1_3.nnet ../resources/properties/acas_property_4.txt 
