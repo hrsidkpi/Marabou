c22aa2e9c6a106fe472ac5c04722a34d639aa26c
JordNGhelfN <yardenghelfan41@gmail.com>
2022-07-30 12:51:11 +0300

    run with 32

---
+++ build_and_run_some.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+
+echo "building..."
+cmake --build . -j 4
+
+cd ../run_batches
+ls
+echo "running all..."
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_1_p_1.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_1_p_2.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_1_p_3.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_1_p_4.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_1_p_sat.sh
+# 
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_2_p_1.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_2_p_2.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_2_p_3.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_2_p_4.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_2_p_sat.sh
+# 
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_3_p_1.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_3_p_2.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_3_p_3.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_3_p_4.sh
+# sbatch --mem=400m -c4 --time=10:0:0 n_1_3_p_sat.sh
+
+sbatch --mem=2g -c4 --time=10:0:0 n_general.sh 1 1 1
+
+sbatch --mem=2g -c4 --time=10:0:0 n_general.sh 2 2 2
+
+sbatch --mem=2g -c4 --time=10:0:0 n_general.sh 3 3 3
+
+sbatch --mem=2g -c4 --time=10:0:0 n_general.sh 4 4 4
